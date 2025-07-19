766352400e06f8e99b815683cba54f6360450e45
sourav kundu <kunduso@amazon.com>
2025-07-18 23:50:21 -0500

    update resource reference

--- ssm.tf
+++ ssm.tf
@@ -2,8 +2,8 @@ resource "aws_ssm_parameter" "nat_gateway_public_ips" {
   name  = "/github-self-hosted-runner-ip-address"
   type  = "StringList"
   value = join(",", [for nat in module.vpc.nat_gateway : nat.public_ip])
-  
+
   tags = {
-    Name = "github-self-hosted-runner-ip-addresses"
+    Name = "${var.name}-ip-addresses"
   }
 }
