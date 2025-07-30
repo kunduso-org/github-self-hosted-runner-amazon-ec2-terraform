b7487da406cbbd06b5a300f23418430f51b25cb1
sourav kundu <kunduso@amazon.com>
2025-07-29 21:34:14 -0500

    create backend to store state file

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "kunduso-terraform-remote-bucket"
+    bucket  = "kunduso-terraform-state-us-west-2"
     encrypt = true
     key     = "tf/github-self-hosted-runner-amazon-ec2-terraform/terraform.tfstate"
-    region  = "us-east-2"
+    region  = "us-west-2"
   }
 }
