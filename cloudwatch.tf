301cc87f1e4532528de7ca0687eaeab6f928310e
sourav kundu <kunduso@amazon.com>
2025-07-30 19:06:43 -0500

    increased retention to an year

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_log_group" "github_runner" {
   name              = "/github-runner/${var.name}"
-  retention_in_days = 30
-
+  retention_in_days = 356
+  kms_key_id        = aws_kms_key.cloudwatch_kms_key.arn
   tags = {
     Name = "${var.name}-logs"
   }
