2263516c90babfe213a824796a0a8bf3a7032778
sourav kundu <kunduso@amazon.com>
2025-08-03 06:39:09 -0500

    add terraform installation step

--- user_data.sh
+++ user_data.sh
@@ -112,6 +112,13 @@ sh get-docker.sh
 usermod -aG docker ubuntu
 echo "$(date): Docker installed successfully"
 
+# Install Terraform
+echo "$(date): Installing Terraform"
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+apt update && apt install -y terraform
+echo "$(date): Terraform installed successfully"
+
 # Create runner user
 echo "$(date): Creating runner user"
 useradd -m -s /bin/bash runner
