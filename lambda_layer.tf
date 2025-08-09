81aaf6e276e5b988a1a466d069e2f321c7da87c5
sourav kundu <kunduso@amazon.com>
2025-08-09 12:30:01 -0500

    Force rebuild when layer contents change

--- lambda_layer.tf
+++ lambda_layer.tf
@@ -1,8 +1,11 @@
-# Lambda layer for PyJWT dependency
+# Lambda layer for PyJWT and requests dependencies
 data "archive_file" "lambda_layer_pyjwt" {
   type        = "zip"
   source_dir  = "${path.module}/lambda_layer/"
   output_path = "${path.module}/lambda_layer.zip"
+  
+  # Force rebuild when layer contents change
+  excludes = []
 }
 
 resource "aws_lambda_layer_version" "lambda_layer_pyjwt" {
