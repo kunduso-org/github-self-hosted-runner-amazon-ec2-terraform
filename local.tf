b9240cd51ab16aa6e9926b2d22b44c2cf194c733
sourav kundu <kunduso@amazon.com>
2025-08-09 07:47:32 -0500

    separated logs for execution and registration

--- local.tf
+++ local.tf
@@ -1,7 +1,8 @@
 data "aws_caller_identity" "current" {}
 locals {
-  principal_root_arn      = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-  principal_logs_arn      = "logs.${var.region}.amazonaws.com"
-  gh_runner_log_group_arn = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:/github-runner/${var.name}/log"
-  secret_arn              = "arn:aws:secretsmanager:${var.region}:${data.aws_caller_identity.current.account_id}:secret:${var.name}-credentials-v2"
+  principal_root_arn                = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+  principal_logs_arn                = "logs.${var.region}.amazonaws.com"
+  gh_runner_lifecycle_log_group_arn = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:/github-runner/${var.name}/lifecycle"
+  gh_runner_execution_log_group_arn = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:/github-runner/${var.name}/execution"
+  secret_arn                        = "arn:aws:secretsmanager:${var.region}:${data.aws_caller_identity.current.account_id}:secret:${var.name}-credentials-v2"
 }
