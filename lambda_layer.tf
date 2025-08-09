ea810ff2d6d22dc065a37094571a5402b73133f4
sourav kundu <kunduso@amazon.com>
2025-08-09 12:38:27 -0500

    added source code hash to include changes in layer folder

--- lambda_layer.tf
+++ lambda_layer.tf
@@ -3,21 +3,13 @@ data "archive_file" "lambda_layer_pyjwt" {
   type        = "zip"
   source_dir  = "${path.module}/lambda_layer/"
   output_path = "${path.module}/lambda_layer.zip"
-  
-  # Force rebuild by adding a timestamp trigger
-  depends_on = [null_resource.layer_trigger]
-}
-
-# Trigger to force layer rebuild when packages change
-resource "null_resource" "layer_trigger" {
-  triggers = {
-    # This will change when we modify this comment to force rebuild
-    rebuild = "v2-with-requests"
-  }
 }
 
 resource "aws_lambda_layer_version" "lambda_layer_pyjwt" {
   filename            = data.archive_file.lambda_layer_pyjwt.output_path
   layer_name          = "pyjwt"
   compatible_runtimes = ["python3.12"]
+  
+  # Force new version when archive changes
+  source_code_hash = data.archive_file.lambda_layer_pyjwt.output_base64sha256
 }
