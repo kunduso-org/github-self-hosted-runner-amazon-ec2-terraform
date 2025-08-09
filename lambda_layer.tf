32f92dde00730889e7fa98d4c00d48fd019d7031
sourav kundu <kunduso@amazon.com>
2025-08-09 12:34:11 -0500

    trigger rebuild

--- lambda_layer.tf
+++ lambda_layer.tf
@@ -4,8 +4,16 @@ data "archive_file" "lambda_layer_pyjwt" {
   source_dir  = "${path.module}/lambda_layer/"
   output_path = "${path.module}/lambda_layer.zip"
   
-  # Force rebuild when layer contents change
-  excludes = []
+  # Force rebuild by adding a timestamp trigger
+  depends_on = [null_resource.layer_trigger]
+}
+
+# Trigger to force layer rebuild when packages change
+resource "null_resource" "layer_trigger" {
+  triggers = {
+    # This will change when we modify this comment to force rebuild
+    rebuild = "v2-with-requests"
+  }
 }
 
 resource "aws_lambda_layer_version" "lambda_layer_pyjwt" {
